b3cda4da4dc0c7a0124ba7f513f90ebfd5a0cde6
Vaadasch <qqqrno@gmail.com>
2022-04-01 00:41:46 +0200

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -5,21 +5,24 @@ if [ -z $PCLOUD_LOGIN ] ; then
         exit 0
 fi
 
-if [ -f /home/$USER/my_password.txt ] ; then
-        pcloudcc -u $PCLOUD_LOGIN -s -p < /home/$USER/my_password.txt &
+if [ -f /my_password.txt ] ; then
+        echo "Connection using the password"
+        pcloudcc -u $PCLOUD_LOGIN -s -p < /my_password.txt &
 else
+        echo "Connection without password"
         pcloudcc -u $PCLOUD_LOGIN &
 fi
 
-sleep 1
+echo "Waiting 20 secondes to pCloud to scan and check if fusermount is here"
+sleep 20
 
-if [ $(ps aux | grep pCloudDr | grep -v grep | wc -l) != 2 ] ; then
+if ! pgrep -x "fusermount" > /dev/null ; then
         echo "Unable to start pCloudCC"
         exit 0
 fi
 
 while [ 1 ] ; do
         sleep 2
-        /bin/bash /home/pcloud/script.sh
+        /bin/bash /script.sh
 done
 
