89641ee1d1240ac85253e7dd8c58e5864ba4a9bf
Vaadasch <qqqrno@gmail.com>
2022-04-01 10:54:49 +0200

    Update script.sh

--- scripts/script.sh
+++ scripts/script.sh
@@ -4,10 +4,13 @@ dt=$(date -Iminutes | awk -F '+' '{print $1}' | sed 's#T#_#')
 
 [ -z "$BACKUP_DIR" ] && BACKUP_DIR="Automated_saves"
 
-for file in /data/* ; do
+#for file in /data/* ; do
+find /data -maxdepth 1 -mindepth 1 -print0 | while IFS= read -r -d '' file; do
+        [ -e "$file" ] || continue
         name=${file##*/}
-        mkdir -p /pdrive/$BACKUP_DIR/$name
-        tar -zcf /pdrive/$BACKUP_DIR/$name/${name}_${dt}.tar /data/$name
-        rm -rf /data/$name
+        [ -z "$name"] || continue
+        mkdir -p "/pdrive/$BACKUP_DIR/$name"
+        tar -zcf "/pdrive/$BACKUP_DIR/$name/${name}_${dt}.tar.gz" "$file"
+        rm -rf "$file"
 done
 
